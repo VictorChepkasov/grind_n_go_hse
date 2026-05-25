6d6c5c0c5b9e468cb48f66c430f59cd823296425
Victor Chepkasov <92711919+VictorChepkasov@users.noreply.github.com>
2026-05-25 17:34:04 +0500

    Delete grind_go_mobile directory

--- grind_go_mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:grind_go_mobile/main.dart';
-
-void main() {
-  testWidgets('App renders title', (WidgetTester tester) async {
-    await tester.pumpWidget(const GrindGoApp());
-    expect(find.text('Grind&GO HSE'), findsOneWidget);
-  });
-}
